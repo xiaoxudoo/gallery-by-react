db6a0cdc07815e6bbca3acfc9a239ada8587d50b
肖旭东 <xiaoxudong@cftig.com>
2016-06-22 17:12:41 +0800

    finish

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './components/Main';
-import GalleryByReactApp from './components/GalleryByReactApp';
-
-// Render the main component into the dom
-ReactDOM.render(<GalleryByReactApp />, document.getElementById('app'));
